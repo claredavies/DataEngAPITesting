5d275aa78bf03d719a2092a3b3975ea253fd4b86
claredavies <91204973+claredavies@users.noreply.github.com>
2021-11-24 18:50:33 +0200

    Delete AnomalyDetection directory

--- AnomalyDetection/TestCase.py
+++
@@ -1,7 +0,0 @@
-class TestCase:
-    def __init__(self, request_type, request_uri, request_body, response_code, response_time_microseconds):
-        self.request_type = request_type
-        self.request_uri = request_uri
-        self.request_body = request_body
-        self.response_code = response_code
-        self.response_time_microseconds = response_time_microseconds
