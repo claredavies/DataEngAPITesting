9e4f912c398ca1b5755bbe0e41945932f598323e
claredavies <clare.davies@abo.fi>
2021-11-25 14:55:59 +0200

    Adding comment to ParseTestCases.py

--- APITest/ParseTestCases/ParseTestCases.py
+++ APITest/ParseTestCases/ParseTestCases.py
@@ -8,7 +8,7 @@ from TestResponse import TestResponse
 
 list_test_cases = []
 
-
+# Method which extracts the request_type, request_uri, request_body & request_time (stored as TestRequest object)
 def extract_sending_request_info(current_line):
     found = current_line.rstrip()
     splitted = found.split()
@@ -19,26 +19,31 @@ def extract_sending_request_info(current_line):
         request_type = splitted[3]
         request_type = request_type[1:]
         request_uri = splitted[4]
+        # There is a body if 'application/json\\r\\nContent-Length' is contained in the line found and contains { & }
         if 'application/json\\r\\nContent-Length' in current_line:
             found = found.replace(request_uri, '')
             first = found.find("{")
             second = current_line.rfind("}")
             if first != -1 and second != -1:
+                # if here contains 'application/json\\r\\nContent-Length', { & } (has request body)
                 body = found[(first - 1) + 1:second]
+                # Remove un-needed \n,\r and ' from body
                 body = body.replace("\\n", "")
                 body = body.replace("\\r", "")
                 body = body.replace("'", "")
+        # Store info extracted as TestRequest Object
         current_test_request = TestRequest()
         current_test_request.request_type = request_type
         current_test_request.request_uri = request_uri
         current_test_request.request_body = body
         current_test_request.request_time = time_sent
         return current_test_request
+    # Line to handle certain cases where line contains sending but not an actual request
     except IndexError:
         print("invalid request:  \n")
         return None
 
-
+# Method which extracts the response code and response time (stored as TestResponse object)
 def extract_response_request_info(current_line):
     found = current_line.rstrip()
     splitted = found.split()
@@ -50,15 +55,18 @@ def extract_response_request_info(current_line):
     current_test_response.response_time = response_time
     return current_test_response
 
-
+# Method which converts the date text to needed datetime format and then calculates time
+# between when request sent and received in microseconds
 def get_reponse_time_microseconds(request_time_sent, response_time_received):
     time_received = datetime.strptime(response_time_received, '%H:%M:%S.%f')
     time_sent = datetime.strptime(request_time_sent, '%H:%M:%S.%f')
     response_time = time_received - time_sent
     return response_time.microseconds
 
-
+# Main function which reads in restler_output.txt file and extracts the test case (request_type, request_uri, request_body, response_code and
+# response_time_microseconds)
 def main():
+    # Reading in file
     script_dir = os.path.dirname(__file__)
     rel_path = "restler_output.txt"
     abs_file_path = os.path.join(script_dir, rel_path)
@@ -67,15 +75,20 @@ def main():
         test_request = TestRequest()
         test_response = TestResponse()
         for line in fp:
+            # Extracting sent part of request
             if "Sending" in line:
                 test_request = extract_sending_request_info(line)
+            # Extracting received part of request (following line)
             if "Received" in line:
                 test_response = extract_response_request_info(line)
+                # Getting time between sent and received request
                 time_microsecond = get_reponse_time_microseconds(test_request.request_time, test_response.response_time)
                 test_case = TestCase(test_request.request_type, test_request.request_uri, test_request.request_body,
                                      test_response.response_code, time_microsecond)
+                # Adding to list of test cases
                 list_test_cases.append(test_case)
 
+    # Storing list as dataframe (easy to view info and export to CSV)
     df = pd.DataFrame([t.__dict__ for t in list_test_cases])
     print(df.head())
     df.to_csv('test_cases_produced.csv')
